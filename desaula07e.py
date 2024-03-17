93f05cce99a14ea2b40e652840b9517b4eb251aa
Joao Pedro Coelho <joaopedrocoepessoal@gmail.com>
2024-03-16 21:19:34 -0300

    Tabuada de um determinado número
    Fiz com que, neste código, mostrasse a tabuada de qualquer número escolhido pelo usuário.

---
+++ desaula07e.py
@@ -0,0 +1,3 @@
+n1 = int(input('Digite um numero: '))
+tab = n1 * 1, n1 * 2, n1 * 3, n1 * 4, n1 * 5, n1 * 6, n1 * 7, n1 * 8, n1 * 9, n1 * 10
+print ('O numero digitado foi {} e sua tabuada é {}'.format(n1, tab))
